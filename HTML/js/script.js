a92a00297675062f432273e4e6b3f94430d25d1c
Sonam <sonam@tothenew.com>
2016-10-28 19:21:30 +0530

    work on HTML and scrolling part

--- HTML/js/script.js
+++ HTML/js/script.js
@@ -1,3 +1,19 @@
-/**
- * Created by TTNd on 26-Oct-16.
- */
+
+$(window).scroll(function() {
+    var scroll = $(window).scrollTop();
+    if (scroll > 0) {
+        $(".header").addClass("sticky");
+    } else {
+        $(".header").removeClass("sticky");
+    }
+    if(scroll < 1510) {
+        $('.rating-star').hide();
+    } else {
+        $('.rating-star').show();
+    }
+    if(scroll > 1992) {
+        $('.thirdcontainer-inr').addClass('decription-container-full')
+    } else {
+        $('.thirdcontainer-inr').removeClass('decription-container-full')
+    }
+}); 
