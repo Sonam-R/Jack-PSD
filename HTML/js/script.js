226a23ccf1e3b174c2f0f5d69c32476e043284ad
Sonam <sonam@tothenew.com>
2016-10-29 05:51:59 +0530

    work on HTML

--- HTML/js/script.js
+++ HTML/js/script.js
@@ -1,6 +1,7 @@
 
 $(window).scroll(function() {
-    var scroll = $(window).scrollTop();
+    var scroll = $(window).scrollTop(),
+        windowWidth = $(window).width();
     if (scroll > 0) {
         $(".header").addClass("sticky");
     } else {
@@ -11,9 +12,34 @@ $(window).scroll(function() {
     } else {
         $('.rating-star').show();
     }
-    if(scroll > 1992) {
-        $('.thirdcontainer-inr').addClass('decription-container-full')
+    if(windowWidth >= 800) {
+        if (scroll > 1992) {
+            $('.thirdcontainer-inr').addClass('decription-container-full')
+        } else {
+            $('.thirdcontainer-inr').removeClass('decription-container-full')
+        }
     } else {
-        $('.thirdcontainer-inr').removeClass('decription-container-full')
+        $('.thirdcontainer-inr').addClass('decription-container-half')
     }
-}); 
+});
+$( document ).ready(function() {
+    $(".menu-toggle").on("click", function () {
+        $('.navigation').slideToggle();
+    });
+    $(".fixed-btn a").on("click", function () {
+        $('.fixed-form').slideToggle();
+    });
+
+
+
+
+    $(document).mouseup(function (e)
+    {
+        var container = $(".fixed-form");
+        if (!container.is(e.target)
+            && container.has(e.target).length === 0)
+        {
+            container.hide();
+        }
+    });
+});
